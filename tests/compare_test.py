abb25bc4ca8a1716618632ac50061dab56b12235
JamesSBeedle <jamessbeedle@gmail.com>
2021-01-21 09:10:43 +0000

    second commit

--- tests/compare_test.py
+++ tests/compare_test.py
@@ -7,7 +7,11 @@ class TestCompare(unittest.TestCase):
 
     def test_compare_3_1_returns_3_is_greater_than_1(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_less_than(self):
         self.assertEqual("3 is less than 5", compare(3, 5))
+
+    def test_comapre_equal(self):
         self.assertEqual("both numbers are equal", compare(10, 10))
 
     
